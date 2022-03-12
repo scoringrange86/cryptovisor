f363ad0e4492ce151fa9665041af58a43547d23b
Steve <scoringrange86@gmail.com>
2022-03-12 13:52:11 -0500

    Commenting Themes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,10 +29,10 @@ pageBackground: "black"
 // tagLineColor: "lavender"
 }
 
-const themes = {
-  light: LightTheme,
-  dark: DarkTheme,
-}
+// const themes = {
+//   light: LightTheme,
+//   dark: DarkTheme,
+// }
 
 function App() {
    const [theme, setTheme] = useState('light');
